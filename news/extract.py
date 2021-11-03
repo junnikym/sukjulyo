595f62e9a0bb8c076b5e1dfa9b9eb2904352937b
junnikym <junnikym@gmail.com>
2021-11-03 23:58:04 +0900

    [UPDATE] news extract - send req to server

--- news/extract.py
+++ news/extract.py
@@ -1,13 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+import json
+
 from rss_parser import RssParser
-from ner import NER
+#from ner import NER
+
+SERVER_URL = 'http://localhost:8080'
 
 rss_parser = RssParser()
-ner = NER()
+#ner = NER()
 
 result = rss_parser.extraction()
+print(len(result))
 for article in result:
 	text = f"{article['title']}\n{article['description']}"
-	list_of_ner_word, decoding_ner_sentence = ner.predict(text)
 	
-	print(list_of_ner_word)
-	print("\n\n")
+	cmd = f'''
+		curl --location --request POST '{SERVER_URL}/news' \
+		--header 'Content-Type: application/json; charset=UTF-8' \
+		--data-raw '{{
+			"corp":"{article['corp']}",
+			"title":"{article['title']}",
+			"link":"{article['link']}",
+			"description":"{article['description']}",
+			"author":"{article['author']}",
+			"pubDate":"{article['pubdate']}"
+		}}'
+	'''
+	
+	pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err = pipe.communicate()
+
+	state = out.decode().find('state')
+
+	if state != -1:  # ERROR
+		print(out)
--- news/rss_parser.py
+++ news/rss_parser.py
@@ -1,28 +1,31 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import datetime
 
 import pandas as pd
 import numpy as np
 
 import argparse
 import json 
-from xml.etree import ElementTree   # xml 파싱 모듈 import
+from bs4 import BeautifulSoup as bf
+from html.parser import HTMLParser
+
+import datetime
+from dateutil.parser import parse as DateParse
 
 if sys.version_info[0] == 3:
-    from urllib.request import urlopen
+	from urllib.request import urlopen
 else:
-    from urllib import urlopen
+	from urllib import urlopen
 
 class RssParser:
 
 	def __init__(
 		self,
 		rss_urls_json_file = './rss.json',
-		search_list_single = ['title', 'link', 'description', 'author', 'pubDate'],
+		search_list_single=['title', 'link', 'description', 'author', 'pubdate'],
 		search_list_multi = ['category',],
-		default_values = {'title':'', 'link':'','description':'','author':None,'pubDate':datetime.datetime.now(),'category':[]},
+		default_values = {'title':'', 'link':'','description':'','author':None,'pubdate':datetime.datetime.now(),'category':[]},
 		print_err = True
 	) -> None:
 		self.rss_urls_json_file	= rss_urls_json_file
@@ -31,7 +34,12 @@ class RssParser:
 		self.default_values		= default_values
 		self.print_err 			= print_err
 
+
+
 	def open_rss_url(self, url, rss_info):
+		'''
+		Load RSS contents through the RSS URL provided by the press
+		'''
 		try:
 			response = urlopen(url).read()
 		except:
@@ -44,30 +52,45 @@ class RssParser:
 		except:
 			response = response.decode('CP949')
 
-		xml_tree = ElementTree.fromstring(response)
+		soup = bf(response, features="html.parser")
 		
-		return list(xml_tree.iter('item'))
+		return list(soup.findAll('item'))
 
 
-	def fine_elems_on_article(self, item):
+
+	def text_parser(self, text):
+		'''
+		Convert HTML Entity to Normal Latter
+		'''
+		html_parser = HTMLParser()
+		text = html_parser.unescape(text)
+		return text \
+			.replace('\\', '\\\\') \
+			.replace("\"", "\\\"") \
+			.replace("\'", "\\'") \
+
+	def fine_elems_on_article(self, item, date_form=['pubdate']):
 		result = {}
 
 		for target in self.search_list_single:
 			elem = item.find(target)
 
 			if (elem != None) and (elem.text != None) :
-				result[target] = elem.text;
+				if target in date_form:
+					result[target] = DateParse(elem.text).date().strftime('%Y-%m-%d %H:%M:%S')
+				else:
+					result[target] = self.text_parser(elem.text)
 			else:
-				if(target == 'pubDate'):
-					result[target] = self.default_values[target].strftime('%Y-%m-%d')
+				if target in date_form:
+					result[target] = self.default_values[target].strftime('%Y-%m-%d %H:%M:%S')
 				else:
 					result[target] = self.default_values[target]
 
 		for target in self.search_list_multi:
-			elem = item.findall(target)
+			elem = item.findAll(target)
 
 			if (elem != None) and (len(elem) != 0) :
-				result[target] = [x.text for x in elem];
+				result[target] = [self.text_parser(x.text) for x in elem]
 			else:
 				result[target] = self.default_values[target]
 
@@ -79,7 +102,7 @@ class RssParser:
 		with open(self.rss_urls_json_file) as json_file:
 			rss_infos = json.load(json_file)
 			
-			for rss_info in rss_infos:		
+			for rss_info in rss_infos:
 				articles = self.open_rss_url(rss_info['url'], rss_info)
 				if articles:
 					for item in articles:
